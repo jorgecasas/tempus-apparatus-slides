1007eaf20ed99a0045f691f7959e003ac81d62a0
Jorge Casas <jorgecasas@iternova.net>
2016-02-04 16:57:40 +0100

    Initializing templates for webinars slides

---
+++ slide_config.js
@@ -0,0 +1,44 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'SmartRoads',
+    subtitle: 'Sistema de Gestión Web de Infraestructuras',
+    eventInfo: {
+      title: 'Webinar: SmartRoads - Gestor de Expedientes',
+      date: '2016-Q1'
+    },
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    favIcon: 'images/favicon.ico',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Soporte Técnico',
+    company: 'ITERNOVA',
+    //gplus: 'http://plus.google.com/1234567890',
+    email: 'soportetecnico@iternova.net',
+    twitter: '@iternova',
+    www: 'https://www.iternova.net',
+    blog: 'https://www.tecnocarreteras.es',
+  //github: 'https://github.com/jorgecasas'
+  }/*, {
+    name: 'Second Name',
+    company: 'Job Title, Google',
+    gplus: 'http://plus.google.com/1234567890',
+    email: 'email@email.com',
+    twitter: '@yourhandle',
+    www: 'http://www.you.com',
+    blog: 'http://blog.you.com',
+    github: 'http://github.com/you'
+  }*/]
+};
+
