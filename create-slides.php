58ed0a1a5417f99d75d4234869c384f8bd53b534
Jorge Casas <jorgecasas@iternova.net>
2017-08-08 14:00:14 +0200

    Updated create-slides.php to allow videos in MP4 format

--- create-slides.php
+++ create-slides.php
@@ -1,13 +1,12 @@
 <?php
 /**
  * Idea: Crear automaticamente los slides a partir de imagenes, parseando su nombre
+ * @version 2017-08-08
+ * @author Jorge Casas [jorgecasas@iternova.net]
  */
 // Variables configuracion
 $path_images = dirname( __FILE__ ) . '/images/others/';
 $path_html = dirname( __FILE__ ) . '/';
-
-// Mostramos texto de ayuda debajo del titulo de cada foto? Por defecto, para adivinanzas, dejarlo a FALSE
-$display_help = false; 
 
  // Limpiamos el array para que no de errores 
 $array_images = scandir( $path_images ); 
@@ -19,25 +18,30 @@ if ( empty( $array_images ) ) {
 $str = file_get_contents( $path_html . 'index-start.html');
 
 // Recorremos todas las imagenes
-$array_extensions_valid = array( 'jpg', 'png', 'gif');
+$array_extensions_valid = array( 'jpg', 'png', 'gif', 'mp4');
 foreach( $array_images as $image ) {
 	$pathinfo = pathinfo($image);
 	if ( in_array( strtolower($pathinfo['extension']), $array_extensions_valid)) {
+        $is_video = ( $pathinfo['extension'] === 'mp4' );
 		echo "\nProcessing ". $pathinfo['basename'];
 		$filename_exploded = explode( '-', strtolower($pathinfo['filename']) );
 		$filename_exploded_ucfirst = strtoupper( implode( ' ' , array_map( 'ucfirst', $filename_exploded ) ) );
 		$str.= '<slide>
             <hgroup>
-                <h2>'.$filename_exploded[ 0 ] . ( $filename_exploded[ 1 ] == 'circa' ? ' - Circa' : '' ).'</h2>';
-        if ( $display_help ) { // Mostramos texto de ayuda debajo del titulo
-            $str.= '<div>' . $filename_exploded_ucfirst . '</div>';
-        }
-        $str.= '</hgroup>
+                <h2>'.$filename_exploded[ 0 ] . ( $filename_exploded[ 1 ] == 'circa' ? ' - Circa' : '' ).'</h2>
+            </hgroup>
             <article>
                 
-                <div class="flexbox vcenter">
-                    <img src="images/others/'.$pathinfo['basename'].'" class="large" alt="'.$filename_exploded_ucfirst.'" title="'.$filename_exploded_ucfirst.'" />
-                </div>
+                <div class="flexbox vcenter">';
+        if ( $is_video ) {
+            $str.= '<video width="640" height="480" controls>
+                    <source src="images/others/'.$pathinfo['basename'].'" type="video/mp4">
+                    Video no soportado
+                </video>';
+        } else {
+            $str.= '<img src="images/others/'.$pathinfo['basename'].'" class="large" alt="'.$filename_exploded_ucfirst.'" title="'.$filename_exploded_ucfirst.'" />';
+        }
+        $str.= '</div>
             </article>
         </slide>'. "\n\n";
 	}
